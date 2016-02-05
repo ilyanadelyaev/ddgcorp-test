7e62f540e07114d52fbe1bb7e429123779e5287e
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2016-02-05 14:59:29 +0300

    add api for tasks

--- ddgcorp/api/urls.py
+++ ddgcorp/api/urls.py
@@ -4,10 +4,24 @@ import ddgcorp.api.views
 
 
 urlpatterns = [
+    # handlers
+    django.conf.urls.url(
+        r'^last_modified$',
+        ddgcorp.api.views.API.last_modified,
+        name='last_modified'),
+    # REST
+    django.conf.urls.url(
+        r'^status/$',
+        ddgcorp.api.views.API.statuses,
+        name='statuses'),
     django.conf.urls.url(
         r'^task/(?P<pk>[0-9]+)/$',
         ddgcorp.api.views.API.task,
         name='task'),
+    django.conf.urls.url(
+        r'^task/(?P<pk>[0-9]+)/status/$',
+        ddgcorp.api.views.API.task_status,
+        name='task_status'),
     django.conf.urls.url(
         r'^task/$',
         ddgcorp.api.views.API.tasks,
--- ddgcorp/api/views.py
+++ ddgcorp/api/views.py
@@ -1,6 +1,10 @@
 import json
+import calendar
 
 import django.http
+
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 import ddgcorp.models
 
@@ -11,13 +15,50 @@ class API(object):
         return django.http.HttpResponse(data, content_type='application/json')
 
     @classmethod
+    def last_modified(cls, request):
+        """
+        """
+        status_last_modified = ddgcorp.models.get_max_audit_log_action_date(
+            ddgcorp.models.Status)
+        task_last_modified = ddgcorp.models.get_max_audit_log_action_date(
+            ddgcorp.models.Task)
+        last_modified = max(status_last_modified, task_last_modified)
+        last_modified = calendar.timegm(last_modified.timetuple())
+        return cls._response(json.dumps({'timestamp': last_modified}))
+
+    @classmethod
+    def statuses(cls, request):
+        ret = [o.to_dict() for o in ddgcorp.models.Status.objects.all()]
+        return cls._response(json.dumps(ret))
+
+    #@require_http_methods(['GET'])
+    @classmethod
     def task(cls, request, pk):
         obj = ddgcorp.models.Task.objects.filter(pk=pk).first()
         if not obj:
             raise django.http.Http404('Task {} does not exist'.format(pk))
         return cls._response(json.dumps(obj.to_dict()))
 
+    #@require_http_methods(['PUT'])
+    @classmethod
+    @csrf_exempt
+    def task_status(cls, request, pk):
+        data = json.loads(request.body)
+        new_status_id = data['new_status_id']
+        obj = ddgcorp.models.Task.objects.filter(pk=pk).first()
+        if not obj:
+            raise django.http.Http404('Task {} does not exist'.format(pk))
+        # update status
+        status = ddgcorp.models.Status.objects.filter(pk=new_status_id).first()
+        if not status:
+            raise django.http.Http404('Status {} does not exist'.format(new_status_id))
+        obj.status = status
+        obj.save()
+        #
+        return cls._response(json.dumps({}))
+
+    #@require_http_methods(['GET'])
     @classmethod
     def tasks(cls, request):
-        objs = [o.to_dict() for o in ddgcorp.models.Task.objects.all()]
-        return cls._response(json.dumps(objs))
+        ret = [o.to_dict() for o in ddgcorp.models.Task.objects.all()]
+        return cls._response(json.dumps(ret))
